f5ab36ffe7f8d11bc4f780def84914c48bc10ecb
LPA-974 <97530712+LPA-974@users.noreply.github.com>
2022-03-08 15:21:00 +0100

    Add files via upload

---
+++ test-Stream.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+
+# In[6]:
+
+
+st.title ("streamlite 101")
+
+
+# In[ ]:
+
+
+
+
