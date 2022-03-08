ebfd458f6aa2a6d68e4505eb3ea0158c1c90688a
LPA-974 <97530712+LPA-974@users.noreply.github.com>
2022-03-08 16:37:43 +0100

    Add files via upload

--- ASCAA_Challenge_0228.py
+++ ASCAA_Challenge_0228.py
@@ -31,22 +31,6 @@ st.dataframe(df.describe().transpose().round())
 
 st.dataframe(df.median().round())
 
-df.drop(['Boosts reçus', 'Nombre de Posts', 'Nombre de Commentaires', 'Nombre de likes émis', 'Points'], axis=1))
-
-st.dataframe(df.head(30))
-#scaler=preprocessing.StandardScaler().fit(df)
-#df_scaled=scaler.transform(df)
-#plt.figure(figsize=(20, 10))
-
-# Génération de la matrice des liens
-#Z = linkage(df_scaled, method = 'ward', metric = 'euclidean')
-
-# Affichage du dendrogramme
-#plt.title("Challenge ASCAA Hivernal - Regroupement des équipes")
-#dendrogram(Z, labels = df.index, leaf_rotation = 75, color_threshold = 7)
-#plt.show()
-
-# In[ ]:
 
 
 
