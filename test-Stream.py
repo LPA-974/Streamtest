d229f710729e3a36253e877bc52caf21dce14f71
LPA-974 <97530712+LPA-974@users.noreply.github.com>
2022-03-08 16:12:51 +0100

    Add files via upload

--- test-Stream.py
+++ test-Stream.py
@@ -5,10 +5,10 @@
 
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-from sklearn import preprocessing
-from sklearn.preprocessing import StandardScaler
+#from sklearn import preprocessing
+#from sklearn.preprocessing import StandardScaler
 #import plotly.express as px
 
 
@@ -33,17 +33,18 @@ st.dataframe(df.median().round())
 
 st.dataframe(df=df.drop(['Boosts reçus', 'Nombre de Posts', 'Nombre de Commentaires', 'Nombre de likes émis', 'Points'], axis=1))
 
-scaler=preprocessing.StandardScaler().fit(df)
-df_scaled=scaler.transform(df)
-plt.figure(figsize=(20, 10))
+st.dataframe(df.head(30))
+#scaler=preprocessing.StandardScaler().fit(df)
+#df_scaled=scaler.transform(df)
+#plt.figure(figsize=(20, 10))
 
 # Génération de la matrice des liens
-Z = linkage(df_scaled, method = 'ward', metric = 'euclidean')
+#Z = linkage(df_scaled, method = 'ward', metric = 'euclidean')
 
 # Affichage du dendrogramme
-plt.title("Challenge ASCAA Hivernal - Regroupement des équipes")
-dendrogram(Z, labels = df.index, leaf_rotation = 75, color_threshold = 7)
-plt.show()
+#plt.title("Challenge ASCAA Hivernal - Regroupement des équipes")
+#dendrogram(Z, labels = df.index, leaf_rotation = 75, color_threshold = 7)
+#plt.show()
 
 # In[ ]:
 
