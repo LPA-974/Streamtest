2e14705870d4afaceb5b3ddd9abc2be6f4c4076c
LPA-974 <97530712+LPA-974@users.noreply.github.com>
2022-03-10 11:17:12 +0100

    Add files via upload

--- ASCAA_Challenge_0228.py
+++ ASCAA_Challenge_0228.py
@@ -3,24 +3,25 @@ import streamlit as st
 
 st.title ("Challenge hivernal ASCAA")
 st.subheader('Résultats au 28 Février 2022')
-st.header('Chargement des données')
+
 df=pd.read_csv("Rapport_Hebdo_2802.csv",sep=";", encoding='Latin_1', index_col = 0)
 print(df.info())
 df.head(30)
 
-st.header('Chargement des données')
+st.markdown('Points au 28 Février')
 st.bar_chart(df['Points'])
 
+st.markdown('Distance totale au 28 Février')
 st.bar_chart(df['Distance totale'])
+
 st.write(df)
-st.dataframe(df.head(30))
 
 st.markdown('Calcul des statistiques')
-st.dataframe(df.describe().transpose().round())
+st.write(df.describe().transpose().round())
 
 
 st.markdown('Calcul de la médiane')
-st.dataframe(df.median().round())
+st.write(df.median().round())
 
-st.bar_chart(df['Points'])
+
 
