f91591bd6b994afb3eae0f982d763a5078cd1206
LPA-974 <97530712+LPA-974@users.noreply.github.com>
2022-03-10 11:12:39 +0100

    Add files via upload

--- ASCAA_Challenge_0228.py
+++ ASCAA_Challenge_0228.py
@@ -1,29 +1,17 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[5]:
-
 import pandas as pd
 import streamlit as st
-#import matplotlib.pyplot as plt
 
-#from sklearn import preprocessing
-#from sklearn.preprocessing import StandardScaler
-#import plotly.express as px
-
-
-# In[6]:
-
-
-st.title ("Test Streamlite sur Data ASCAA")
-st.markdown('premier essai sur Streamlite')
-
+st.title ("Challenge hivernal ASCAA")
+st.subheader('Résultats au 28 Février 2022')
+st.header('Chargement des données')
 df=pd.read_csv("Rapport_Hebdo_2802.csv",sep=";", encoding='Latin_1', index_col = 0)
 print(df.info())
 df.head(30)
 
 st.header('Chargement des données')
+st.bar_chart(df['Points'])
 
+st.bar_chart(df['Distance totale'])
 st.write(df)
 st.dataframe(df.head(30))
 
@@ -36,29 +24,3 @@ st.dataframe(df.median().round())
 
 st.bar_chart(df['Points'])
 
-df=df.drop(['Boosts reçus', 'Nombre de Posts', 'Nombre de Commentaires', 'Nombre de likes émis', 'Points'], axis=1)
-
-st.image('./Dendro_0228.jpg')
-
-st.dataframe(df.head(30))
-
-st.button('Click me')
-
-st.checkbox('I agree')
-
-st.radio('Pick one', ['cats', 'dogs'])
-
-st.multiselect('Buy', ['milk', 'apples', 'potatoes'])
-
-st.slider('Pick a number', 0, 100)
-st.select_slider('Pick a size', ['S', 'M', 'L'])
-st.text_input('First name')
-st.number_input('Pick a number', 0, 10)
-st.text_area('Text to translate')
-st.date_input('Your birthday')
-st.time_input('Meeting time')
-st.file_uploader('Upload a CSV')
-st.camera_input('Take a picture')
-
-
-st.color_picker('Pick a color')
